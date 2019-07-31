36a3b7a77fd26637cc56b80df061168c3a94355c
Amit Singh <amit.singh1@zensar.com>
2019-07-31 14:40:48 +0530

    Remove debugger

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -11,7 +11,6 @@ export class AuthGuard implements CanActivate{
 canActivate(route:ActivatedRouteSnapshot,
             state:RouterStateSnapshot):Observable<boolean> | Promise<boolean> | boolean {
               return this.fakeService.isAuthorised().then((authenticated: boolean)=>{
-                debugger;
                 if(authenticated){
                   return true;
                 }
