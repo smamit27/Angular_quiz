f2cd3a1e8b9f606f1e449b8eeb1e809558472931
smamit27 <smamit27@gmail.com>
2019-08-16 20:51:48 +0530

    Update test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,7 +30,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    debugger;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to quiz!');
   });
 });
