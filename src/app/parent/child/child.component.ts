f95d306dcfdd9b987ea0c0eca63c75e683277f1f
smamit27 <smamit27@gmail.com>
2019-09-02 22:02:08 +0530

    Style added and update child component

--- src/app/parent/child/child.component.ts
+++ src/app/parent/child/child.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input,ChangeDetectionStrategy } from '@angular/core';
-import { database } from 'firebase';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-child',
@@ -8,11 +9,43 @@ import { database } from 'firebase';
   changeDetection: ChangeDetectionStrategy.OnPush
 
 })
-export class ChildComponent implements OnInit {
-@Input() data:any;
-  constructor() { }
+export class ChildComponent  {
+  @Input() data: any;
+  userdetail: Array<any> = [];
+  user: object = {
+    firstname: "",
+    lastname: "",
+    age: ""
+  };
+  /* Type Check Here */
+  addUserDetails: boolean = true;
+  updateUserDetails: boolean = false;
+  showUserDetail: boolean = true;
 
-  ngOnInit() {
+  addUser(formdata: NgForm) {
+    if (formdata !== null || formdata !== undefined) {
+      this.showUserDetail = false;
+    }
+    this.userdetail.push(formdata.value);
+    formdata.reset();
+  }
+  editUser(data:any) {
+    this.user = data;
+    this.addUserDetails = false;
+    this.updateUserDetails = true;
+  }
+  deleteUser(i:any) {
+    this.userdetail.splice(i, 1);
+    if (this.userdetail.length == 0) {
+      this.showUserDetail = true;
+
+    }
+  }
+  updateUser(data:any) {
+    this.updateUserDetails = false;
+    this.addUserDetails = true;
+    this.user = {};
+
   }
 
 
